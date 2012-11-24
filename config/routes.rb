3bb7a8e87cd49fa74f598dec1fb6d9e351af1e9b
futuresandwich <lorenzon@gmail.com>
2012-11-25 02:10:33 +0700

    fixing home

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ KinyeiPos::Application.routes.draw do
 
   root to: 'static_pages#home'
 	
-	match '/', to: 'static_pages#home'
+	match '/', to: 'stories#index'
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
