b358d977ecb1adab5b7d717f8eb992a1790b3adc
futuresandwich <lorenzon@gmail.com>
2013-01-06 12:17:41 +0700

    working on ratings

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121129075921) do
+ActiveRecord::Schema.define(:version => 20130101060820) do
 
   create_table "games", :force => true do |t|
     t.integer  "user_id"
@@ -55,6 +55,15 @@ ActiveRecord::Schema.define(:version => 20121129075921) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+  end
+
+  create_table "ratings", :force => true do |t|
+    t.integer  "user_id"
+    t.integer  "story_id"
+    t.integer  "score"
+    t.string   "comment"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "relationships", :force => true do |t|
