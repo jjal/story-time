85409c2478d54b884f9afdfa38d769545295a81b
futuresandwich <lorenzon@gmail.com>
2013-01-08 22:24:28 +0700

    adding ratings notification

--- app/assets/javascripts/rating.js
+++ app/assets/javascripts/rating.js
@@ -1,6 +1,15 @@
 $(document).ready(function() {
+    
+    $('form.rating').submit(function() {
+      $('form.rating').children('#status').fadeIn('slow', function() {
+      $('form.rating').children('#status').delay(3000).fadeOut('slow'); });
+    });
+    
     // Submits the form (saves data) after user makes a change.
     $('form.rating').change(function() {
         $('form.rating').submit();
     });
+    
+    $('form.rating').children('#status').hide();
+    
 });
