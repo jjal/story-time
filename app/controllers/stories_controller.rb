930ef187bfc2981a0bb6dc3fe8739bca14ecd092
futuresandwich <lorenzon@gmail.com>
2013-01-08 23:18:09 +0700

    adding alphabetical ordering to stories and upping micropost limit

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -3,7 +3,7 @@ class StoriesController < ApplicationController
   before_filter :correct_user,   only: [:destroy, :edit, :update]
   
   def index
-    @stories = Story.paginate(page: params[:page], per_page: 10)
+    @stories = Story.paginate(page: params[:page], per_page: 10, order: :title)
   end 
   
 	def show
