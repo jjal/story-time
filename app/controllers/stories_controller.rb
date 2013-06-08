bcbcaf3b75f9f6ec3f62903c96c9f89f34b3c74c
futuresandwich <lorenzon@gmail.com>
2013-06-08 18:13:54 +0200

    merge

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,53 +1,58 @@
+require 'will_paginate/array'
+require 'core_ext/array'
+
 class StoriesController < ApplicationController
   before_filter :signed_in_user, only: [:edit, :update, :new, :create, :graph, :destroy]
   before_filter :correct_user,   only: [:destroy, :edit, :update]
   
   def index
-    @stories = Story.paginate(page: params[:page], per_page: 10, order: :title, conditions: 
+    @stories = Story.find(:all, conditions: 
       signed_in? ? 
         (current_user.admin?) ? {} : ["status = 1 or user_id = ?", current_user.id]
       :
         ["status = 1"]
     )
+    @stories = Story.sort_by_score(@stories).paginate(page: params[:page], per_page: 10)
+
     @activity = Micropost.get_recent(10)
   end 
   
-	def show
-		@story = Story.find(params[:id])
-	end
-	
-	def edit
-		@story = Story.find(params[:id])
+  def show
+    @story = Story.find(params[:id])
+  end
+  
+  def edit
+    @story = Story.find(params[:id])
   end
   
   def graph
     @story = Story.find(params[:story_id])
   end
-	
-	def new
-		@story = current_user.stories.build
+  
+  def new
+    @story = current_user.stories.build
   end
-	
-	def create
+  
+  def create
     @story = current_user.stories.build(params[:story])
     if @story.save
-			flash[:success] = "Cool you created a story. Now add some pages."
+      flash[:success] = "Cool you created a story. Now add some pages."
       current_user.microposts.create({content: "#{current_user.name} just created a new story, <a href='#{story_path(@story)}'>'#{@story.title}'</a>" })
       redirect_to edit_story_path(@story)
     else
       render 'new'
     end
   end
-	
-	def update
-		@story = Story.find(params[:id])
+  
+  def update
+    @story = Story.find(params[:id])
     if @story.update_attributes(params[:story])
-			flash[:success] = "Story updated"
+      flash[:success] = "Story updated"
     end
     render 'edit'
   end
-	 
-	def destroy
+   
+  def destroy
     @story = Story.find(params[:id])
     @story.destroy
     flash[:success] = "Story destroyed."
