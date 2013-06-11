05fa4e468409baa14768a78cb19e6cfdc4dd5087
futuresandwich <lorenzon@gmail.com>
2013-06-11 20:40:36 +0200

    fixing migration

--- db/migrate/20130111125635_change_image_urls.rb
+++ db/migrate/20130111125635_change_image_urls.rb
@@ -1,14 +1,14 @@
 class ChangeImageUrls < ActiveRecord::Migration
   def up
   	#this is here because the prod db got out of sync
-  	drop_table :merit_actions
-  	drop_table :merit_activity_logs
-  	drop_table :sashes
-  	drop_table :badges_sashes
-  	drop_table :merit_scores
-    drop_table :merit_score_points
-    remove_column :users, :sash_id
-    remove_column :users, :level
+  	# drop_table :merit_actions
+  	# drop_table :merit_activity_logs
+  	# drop_table :sashes
+  	# drop_table :badges_sashes
+  	# drop_table :merit_scores
+   #  drop_table :merit_score_points
+   #  remove_column :users, :sash_id
+   #  remove_column :users, :level
   end
 
   def down
